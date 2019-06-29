f9ecb4adb92051f26c30446dcaf748a3fb3a34c0
sergorl <serg.neworl@gmail.com>
2019-06-29 22:31:03 +0300

    edited config

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
-sudo /home/box/web/etc/init.d/nginx restart
+sudo /etc/init.d/nginx restart
 
